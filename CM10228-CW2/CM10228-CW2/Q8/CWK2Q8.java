1d188666bf9b2bdc9f94b038e8cdad377e06bf76
Ethan Mansbridge <>
2021-04-12 15:37:24 +0000

    Finished q8

--- CM10228-CW2/CM10228-CW2/Q8/CWK2Q8.java
+++ CM10228-CW2/CM10228-CW2/Q8/CWK2Q8.java
@@ -19,14 +19,36 @@
  *  rules of Project Euler, your solution should return an answer under 
  *  60 seconds.
 */
+import java.lang.Math;
 
 public class CWK2Q8 {
 
-	public static int howManyTuesdays() {
-		
-	}
+  private static int isTuesday(int year, int month, int century){
+    //Algorithm taken from: https://cs.uwaterloo.ca/~alopez-o/math-faq/node73.html
+    int result = (int) Math.round((1 + Math.floor((2.6 * month) - 0.2) - (2 * 19) + year + Math.floor(year / 4) + Math.floor(19 / 4)) % 7);
+    if (result == 6){
+      return 1;
+    } else{
+      return 0;
+    }
+  }
 	
-	public static void main(String[] args) {
+  public static int howManyTuesdays(){
+    int totalTuesdays = 0;
+		for (int i = 1901; i <= 2000; i++){
+      for (int j = 1; j <= 12; j++){
+        if (j == 11 || j == 12){
+          totalTuesdays = totalTuesdays + isTuesday(i - 1, j, 19);
+        } else{
+          totalTuesdays = totalTuesdays + isTuesday(i, j, 19);
+        }
+      }
+    }
+    return totalTuesdays;
+  }
+
+	
+	public static void main(String[] args){
 		int result = CWK2Q8.howManyTuesdays();
 		System.out.println("Number of Tuesdays = " + result);
 	}
